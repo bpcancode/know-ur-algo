101dfe7da0127ac227462490a52b40bd5235ecc2
bpcancode <poudelb891@gmail.com>
2025-01-02 21:21:06 +0545

    api update

--- API/Program.cs
+++ API/Program.cs
@@ -44,11 +44,11 @@ builder.Services.AddAuthentication(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.MapScalarApiReference();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//}
+app.MapOpenApi();
+app.MapScalarApiReference();
 
 app.UseHttpsRedirection();
 
