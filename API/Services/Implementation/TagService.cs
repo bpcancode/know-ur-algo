dbc59fa73ebd263b506063352fa65ec6d0cafbdc
Madhuri <madhurirajbanshi318@gmail.com>
2025-01-02 16:32:47 +0545

    tag complete

---
+++ API/Services/Implementation/TagService.cs
@@ -0,0 +1,22 @@
+﻿using API.Dtos.Request;
+using API.Persistence.Entities;
+using API.Services.Interface;
+using Microsoft.EntityFrameworkCore;
+
+public class TagService : ITagService
+{
+	private readonly DbContext _context;
+
+	public TagService(DbContext context)
+	{
+		_context = context;
+	}
+
+	public async Task<Tag> AddTagAsync(TagDto dto)
+	{
+		var tag = new Tag { Name = dto.Name };
+		_context.Set<Tag>().Add(tag);
+		await _context.SaveChangesAsync();
+		return tag;
+	}
+}
